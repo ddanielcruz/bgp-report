2b71d8915cee2fc73157c35792fbd21cc70229e4
Daniel Cunha <danielcunha54@gmail.com>
2020-12-05 19:09:24 -0300

    refactor: move eslint config to a package

---
+++ packages/eslint-config/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'import-helpers'],
+  rules: {
+    'prettier/prettier': 'error',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    },
+    react: {
+      version: '999.999.999'
+    }
+  }
+}
