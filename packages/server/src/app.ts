800808830370df1a1e80d8f99a35b231aa25e72f
Daniel Cunha <danielcunha54@gmail.com>
2020-12-05 21:51:21 -0300

    feat: create error handler middleware

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import helmet from 'helmet'
 import 'express-async-errors'
 
+import errorHandler from './middleware/errorHandler'
 import logger from './middleware/logger'
 import routes from './routes'
 
@@ -13,5 +14,6 @@ app.use(cors())
 app.use(logger())
 app.use(express.json())
 app.use('/api', routes)
+app.use(errorHandler)
 
 export default app
