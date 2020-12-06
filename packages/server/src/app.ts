8c34bb7e63869458dc3e59efcb72c9bb7e79c457
Daniel Cunha <danielcunha54@gmail.com>
2020-12-05 21:40:07 -0300

    feat: create logger middleware

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -4,11 +4,13 @@ import express from 'express'
 import helmet from 'helmet'
 import 'express-async-errors'
 
+import logger from './middleware/logger'
 import routes from './routes'
 
 const app = express()
 app.use(helmet())
 app.use(cors())
+app.use(logger())
 app.use(express.json())
 app.use('/api', routes)
 
