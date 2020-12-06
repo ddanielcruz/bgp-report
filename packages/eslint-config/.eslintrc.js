d5126044169bde445e36fba76118036575b08bf7
Daniel Cunha <danielcunha54@gmail.com>
2020-12-05 22:43:49 -0300

    chore: update import helpers config

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
       'warn',
       {
         newlinesBetween: 'always',
-        groups: ['module', ['parent', 'sibling', 'index']],
+        groups: ['module', '/^@/', ['parent', 'sibling', 'index']],
         alphabetize: { order: 'asc', ignoreCase: true }
       }
     ]
