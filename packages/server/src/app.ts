3bec77a3f52683891561bb415931416ac9511736
Daniel Cunha <danielcunha54@gmail.com>
2021-01-12 22:07:40 -0300

    feat: add swagger documentation

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,10 +2,12 @@ import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
+import swaggerUI from 'swagger-ui-express'
 import 'express-async-errors'
 
 import './config/schedulers'
 import './database/connection'
+import swaggerConfig from './config/swagger.json'
 import errorHandler from './middleware/errorHandler'
 import logger from './middleware/logger'
 import routes from './routes'
@@ -16,6 +18,7 @@ app.use(cors())
 app.use(logger())
 app.use(express.json())
 app.use('/api', routes)
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerConfig))
 app.use(errorHandler)
 
 export default app
