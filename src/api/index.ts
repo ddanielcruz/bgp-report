32a3b5ecb00218880ae36f6c581d26063f8dddc6
Daniel Cunha <danielcunha54@gmail.com>
2021-02-28 10:33:42 -0300

    docs: add swagger

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,12 @@ import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
+import swaggerUI from 'swagger-ui-express'
 import 'express-async-errors'
 
 import '@database/connection'
 import '@config/schedulers'
+import swaggerConfig from '@config/swagger.json'
 import routes from './routes'
 import { errorHandler, logger } from './middleware'
 
@@ -16,6 +18,7 @@ app.use(cors())
 app.use(logger())
 app.use(express.json())
 app.use('/api', routes)
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerConfig))
 app.use(errorHandler)
 
 export default app
