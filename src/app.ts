044b6ce03b7013c1b951cb82c2d26c17dd7edf18
Daniel Cunha <danielcunha54@gmail.com>
2021-03-08 20:12:58 -0300

    refactor: start socket.io

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
-import { createServer } from 'http'
+import http from 'http'
 import { Server } from 'socket.io'
 import swaggerUI from 'swagger-ui-express'
 import 'express-async-errors'
@@ -15,34 +15,17 @@ import swaggerConfig from './config/swagger.json'
 import { errorHandler, logger } from './middleware'
 import { routes } from './routes'
 
-class App {
-  public express: express.Application
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(logger())
+app.use(express.json())
+app.use('/api', routes)
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerConfig))
+app.use(errorHandler)
 
-  constructor() {
-    this.express = express()
+const server = http.createServer(app)
+const io = new Server(server)
+sockets.configure(io)
 
-    this.middleware()
-    this.routes()
-  }
-
-  middleware() {
-    this.express.use(helmet())
-    this.express.use(cors())
-    this.express.use(logger())
-    this.express.use(express.json())
-  }
-
-  routes() {
-    this.express.use('/api', routes)
-    this.express.use('/', swaggerUI.serve, swaggerUI.setup(swaggerConfig))
-    this.express.use(errorHandler)
-  }
-
-  sockets() {
-    const server = createServer(this.express)
-    const io = new Server(server)
-    sockets.configure(io)
-  }
-}
-
-export default new App().express
+export default server
